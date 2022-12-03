71f002ee73878541ba94327fca39af646ddc9650
Jordy Kristianata <jordykristianata@gmail.com>
2022-12-03 18:34:25 +0700

    fixing ip source

--- controller/api/getIpController.js
+++ controller/api/getIpController.js
@@ -22,35 +22,21 @@ module.exports = {
         };
         // res.json({ myIP: req.socket.remoteAddress });
         res.header('Content-Type', 'application/json');
-        let dataIP = await new Promise((resolve, reject) => {
-            request.get({
-                url: 'https://ipinfo.io?token=e231877e2f20dc',
-                json: true,
-                headers: { 'User-Agent': 'request' }
-            }, (err, res, data) => {
-                if (err) reject(err);
-                if (res.statusCode == 200) {
-                    resolve(data);
-                } else {
-                    reject('error RC ' + res.statusCode);
-                }
-            });
-        });
-        let ip = req.socket.remoteAddress;
-        let lat = await dataIP.loc.split(',')[0];
-        let lon = await dataIP.loc.split(',')[1];
+        let ip = req.query.ip;
+        let lat = req.query.latlong.split(',')[0];;
+        let lon = req.query.latlong.split(',')[1];;
         let address = await getAddress(lat, lon);
-        let accurate_lat = req.query.latlong.split(',')[0];
-        let accurate_lon = req.query.latlong.split(',')[1];
-        let accurate_address = await getAddress(accurate_lat, accurate_lon);
+        let accurateLat = req.query.accuratelatlong.split(',')[0];
+        let accurateLon = req.query.accuratelatlong.split(',')[1];
+        let accurateAddress = await getAddress(accurateLat, accurateLon);
         dbModel.add(req.con, res, {
             table: 'stalker',
             data: {
                 ip: await ip,
                 latlong: lat + ',' + lon,
                 address: address,
-                accurate_latlong: accurate_lat + ',' + accurate_lon,
-                accurate_address: accurate_address,
+                accurate_latlong: accurateLat + ',' + accurateLon,
+                accurate_address: accurateAddress,
                 date_created: new Date() // moment().format(("YYYY-MM-DDTHH:mm:ss"))
             },
             server: true
